aa0871fdd86fd9b1cc4107e5caaae4999a019d78
福本萌々恵 <hakvt0.89io@gmail.com>
2019-04-25 00:23:02 +0900

    EnemyBaseの処理が増えたので、EnemyBaseのcppを作成・分離

---
+++ StarFish_Project/Enemy/Enemy/EnemyBase.cpp
@@ -0,0 +1,60 @@
+﻿#include"EnemyBase.h"
+
+EnemyBase::EnemyBase() {
+	m_pos.x = 0.f;
+	m_pos.y = 0.f;
+	m_dead_timer = 60;
+	m_is_dead = false;
+}
+//―――――――――――――――――――――
+
+void EnemyBase::OutScreen() {
+	// 画面外に出たら、削除までの時間をカウントダウン
+	if (m_pos.y > WINDOW_H_F || m_pos.x<0 || m_pos.x>WINDOW_W_F) {
+		if (m_dead_timer >= 0) {
+			m_dead_timer--;
+			// 時間が0になったら削除フラグをtrueに
+			if (m_dead_timer <= 0) {
+				m_is_dead = true;
+			}
+		}
+	}
+	// 画面内に戻ったら時間を戻す
+	else if (m_pos.y < WINDOW_H_F || m_pos.x>0 || m_pos.x < WINDOW_W_F) {
+		m_dead_timer = 60;
+	}
+}
+//―――――――――――――――――――――
+
+float EnemyBase::GetPosX() {
+	return m_pos.x;
+}
+
+float EnemyBase::GetPosY() {
+	return m_pos.y;
+}
+
+void EnemyBase::SetPosX(float x) {
+	m_pos.x = x;
+}
+
+void EnemyBase::SetPosY(float y) {
+	m_pos.y = y;
+}
+
+float EnemyBase::GetSpeed() {
+	return m_speed;
+}
+
+bool EnemyBase::IsDead() {
+	return m_is_dead;
+}
+
+int EnemyBase::GetDeadTimer() {
+	return m_dead_timer;
+}
+
+int EnemyBase::GetEnemyType() {
+	return m_enemy_type;
+}
+//―――――――――――――――――――――
--- StarFish_Project/Enemy/Enemy/EnemyBase.h
+++ StarFish_Project/Enemy/Enemy/EnemyBase.h
@@ -1,12 +1,10 @@
 ﻿#pragma once
+
+#include"../../Lib/Texture/TextureBoad2D.h"
+#include"../../Lib/Window/Window.h"
 #include"../State/EnemyStateBase.h"
 #include"../State/EnemyWaitState.h"
-#include"../State/EnemyChaseState.h"
-#include"../State/EnemySideMoveState.h"
-#include"../State/EnemyVerticalMoveState.h"
-#include"../../Lib/Texture/Texture.h"
-#include"../../Lib/Texture/TextureBoad2D.h"
-#include"../../Lib/D3D/D3D9.h"
+
 
 enum EnemyTypeId {
 	SeaUrchinId,		// ウニ
@@ -18,73 +16,42 @@ enum EnemyTypeId {
 // 敵基底クラス
 class EnemyBase {
 public:
-	EnemyBase() {}
+	// コンストラクタ内で共通変数の初期化
+	EnemyBase();
 	virtual ~EnemyBase() {}
 
-	virtual void Init() = 0;						// 初期化
+	virtual void Init() = 0;						// 初期化《変更予定》
 	virtual void Update() = 0;						// 更新
 	virtual void Draw() = 0;						// 描画
 	virtual void ChangeState(StateBase* state) = 0;	// 遷移
 
 	// 画面外に出たらm_is_deadをtrueにする関数
-	virtual void  OutScreen() {
-		// 画面外に出たら、削除までの時間をカウントダウン
-		if (m_pos.y > WINDOW_H_F || m_pos.x<0 || m_pos.x>WINDOW_W_F) {
-			if (m_dead_timer >= 0) {
-				m_dead_timer--;
-				// 時間が0になったら削除フラグをtrueに
-				if (m_dead_timer <= 0) {
-					m_is_dead = true;
-				}
-			}
-		}
-		// 画面内に戻ったら時間を戻す
-		else if (m_pos.y < WINDOW_H_F || m_pos.x>0 || m_pos.x < WINDOW_W_F) {
-			m_dead_timer = 60;
-		}
-	}
+	virtual void  OutScreen();
 
 	// 位置座標のゲッター、セッター
-	virtual float GetPosX() {
-		return m_pos.x;
-	}
+	virtual float GetPosX();
+	virtual float GetPosY();
 
-	virtual float GetPosY() {
-		return m_pos.y;
-	}
-
-	virtual void SetPosX(float x) {
-		m_pos.x = x;
-	}
-
-	virtual void SetPosY(float y) {
-		m_pos.y = y;
-	}
+	virtual void SetPosX(float x);
+	virtual void SetPosY(float y);
 
 	// 速度のゲッター
-	virtual float GetSpeed() {
-		return m_speed;
-	}
+	virtual float GetSpeed();
 
 	// 死亡フラグのゲッター
-	virtual bool IsDead() {
-		return m_is_dead;
-	}
+	virtual bool IsDead();
 
-	// タイマーのゲッター、セッター
-	virtual int GetDeadTimer() {
-		return m_dead_timer;
-	}
+	// タイマーのゲッター
+	virtual int GetDeadTimer();
 
 	// 敵種類のゲッター
-	virtual int GetEnemyType() {
-		return m_enemy_type;
-	}
+	virtual int GetEnemyType();
 
 protected:
-	D3DXVECTOR2 m_pos;	// 座標
-	float m_speed;		// 速度
+	// まだObject未反映なので変数はそのまま
+	D3DXVECTOR2 m_pos;	// 座標			《削除予定》
+	float m_speed;		// 速度			《削除予定》
 	int m_dead_timer;	// 削除用タイマー
-	bool m_is_dead;		// 削除フラグ
+	bool m_is_dead;		// 削除フラグ	《削除予定》
 	int m_enemy_type;	// 敵の種類
 };
