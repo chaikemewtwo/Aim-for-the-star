a1088fb8a6bc0052703afca0d0fce06e167676b8
福本萌々恵 <hakvt0.89io@gmail.com>
2019-04-26 15:28:46 +0900

    ほら貝のクラス「SellFish」のcppとhを追加

---
+++ StarFish_Project/Enemy/Enemy/SellFish.cpp
@@ -0,0 +1,33 @@
+﻿#include"SellFish.h"
+
+SellFish::SellFish(float x, float y, bool no_move) {
+	m_pos.x = x;
+	m_pos.y = y;
+	Init();
+}
+//―――――――――――――――――――――――
+
+void SellFish::Init() {
+	m_pstate_base = Wait::GetInstance();
+	
+	m_speed = 5;
+	m_enemy_type = SellFishId;	
+}
+//―――――――――――――――――――――――
+
+void SellFish::Update() {
+	m_pstate_base->Action(this);
+
+	OutScreen();
+}
+//―――――――――――――――――――――――
+
+void SellFish::Draw() {
+	//Texture::Draw2D("", m_pos.x, m_pos.y);
+}
+//―――――――――――――――――――――――
+
+void SellFish::ChangeState(StateBase* state) {
+	m_pstate_base = state;
+}
+//―――――――――――――――――――――――
---
+++ StarFish_Project/Enemy/Enemy/SellFish.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include"EnemyBase.h"
+#include"../../Lib/Window/Window.h"
+
+class SellFish :public EnemyBase {
+public:
+	SellFish(float x, float y, bool no_move = false);
+	~SellFish() {}
+	void Init()override;
+	void Update()override;
+	void Draw()override;
+	void ChangeState(StateBase* state)override;
+private:
+	StateBase* m_pstate_base;
+};
