8222c05f946aea1231c34782bb3632ac8685b11d
岡田　将嗣 <41092537+chaikemewtwo@users.noreply.github.com>
2019-05-06 13:39:21 +0900

    Merge branch 'master' of https://github.com/chaikemewtwo/Aim-for-the-star
    # Conflicts:
    #	StarFish_Project/Player/PlayerBase/PlayerBase.cpp

--- StarFish_Project/Player/PlayerBase/PlayerBase.cpp
+++ StarFish_Project/Player/PlayerBase/PlayerBase.cpp
@@ -1,30 +1,36 @@
 ﻿#include "PlayerBase.h"
+#include "../PlayerState/PlayerWaitState/PlayerWaitState.h"
+#include "../PlayerState/PlayerSwimState/PlayerSwimState.h"
+#include "../PlayerState/PlayerStandingWaitState/PlayerStandingWaitState.h"
 #include <cmath>
 
 
-PlayerBase::PlayerBase() /*:m_state(new WaitState)*/{
+PlayerBase::PlayerBase() :m_state(PlayerWaitState::GetInstance()) {
 	// 移動速度
-	m_move_speed = 2.f;
+	m_speed = 2.f;
 
 	// 傾き
 	m_character_angle = 0.f;
 
-	// 泳ぎコマンドインターバル、初期化時はMAX値
-	m_swim_interval_count = SWIM_INTERVAL;
-
 	// アニメーション番号
 	m_animation_number = 0;
 
+<<<<<<< HEAD
 	// 追加(初期化していなかったので):移動初期化
 	m_move.x = 0.f;
 	m_move.y = 0.f;
 
 	// 初期化時に表示する画像
 	/*m_player_texture[WAIT];*/
+=======
+	// 待機状態初期化
+	m_state->Init(this);
+>>>>>>> player(State)
 }
 
 
 void PlayerBase::Update() {
+<<<<<<< HEAD
 	// HACK:自機2の操作を分離する
 	Keybord& kb = Keybord::getInterface();
 
@@ -68,12 +74,17 @@ void PlayerBase::Update() {
 		// 泳ぎインターバルカウントアップ
 		++m_swim_interval_count;
 	}
+=======
+	// 内部の処理は各ステート内で管理しています
+	m_state->Update(this);
+>>>>>>> player(State)
 }
 
 
 void PlayerBase::Draw() {
 	// 自機2にも自機1のものを使用中
 	// 第7、8引数が0.5fずつで中心座標から描画	
+	// 被弾状態は描画する、しないを切り替えて表現する DamageStateが消えるかも
 	Texture::Draw2D(
 		m_player_texture.c_str(),
 		m_pos.x,
@@ -100,55 +111,38 @@ void PlayerBase::AddGravity() {
 
 void PlayerBase::AngleAdjust(bool is_move_right) {
 	// 自機傾き変更、TRUEで右へ傾く
-	if (m_character_angle < MAX_ANGLE && m_character_angle > -MAX_ANGLE) {
+	if (m_character_angle <= MAX_ANGLE && m_character_angle >= -MAX_ANGLE) {
 		m_character_angle += is_move_right ? ANGLE_ADD : -ANGLE_ADD;
 	}
 	// 角度変更範囲設定
-	else if (m_character_angle > -MAX_ANGLE) {
-		m_character_angle = MAX_ANGLE - 1.f;
+	// 
+	else if (m_character_angle >= MAX_ANGLE) {
+		m_character_angle = MAX_ANGLE;
 	}
-	else if (m_character_angle < MAX_ANGLE) {
-		m_character_angle = -MAX_ANGLE + 1.f;
+	else if (m_character_angle <= MAX_ANGLE) {
+		m_character_angle = -MAX_ANGLE;
 	}
 }
 
 
 void PlayerBase::SwimUp() {
+<<<<<<< HEAD
 	
 	// ベクトルの長さ(上方向への移動)
+<<<<<<< HEAD
+	m_move.x = sin(m_character_angle * PI / (float)180.f) * m_move_speed;
+	m_move.y = cos(m_character_angle * PI / (float)180.f) * m_move_speed;
+=======
+	// 上方向への移動量(ベクトルの長さ)を割り出す
+	m_move.x = sin(m_character_angle * PI / (float)180.f) * m_speed;
+	m_move.y = cos(m_character_angle * PI / (float)180.f) * m_speed;
+>>>>>>> player(State)
+=======
 	m_move.x += sin(m_character_angle * PI / (float)180.f) * m_move_speed;
 	m_move.y += cos(m_character_angle * PI / (float)180.f) * m_move_speed;
+>>>>>>> c1b858d9a26b0737d6c4097bb5cca4fca65fc154
 
 	// 移動量インクリメント
 	//m_pos.x += m_move.x;
 	//m_pos.y -= m_move.y;
 }
-
-
-void PlayerBase::SetAnimationNumber(int new_animation_number) {
-	// アニメーション番号上書き
-	m_animation_number = new_animation_number;
-}
-
-D3DXVECTOR2 PlayerBase::GetPos() {
-	return m_pos;
-}
-
-D3DXVECTOR2 PlayerBase::GetMovePos() {
-	return m_move;
-}
-
-
-void PlayerBase::SetPos(D3DXVECTOR2 pos) {
-	m_pos = pos;
-}
-
-void PlayerBase::SetMovePos(D3DXVECTOR2 move) {
-	m_move = move;
-}
-
-
-//void PlayerBase::ChangeState(STATE* new_state) {
-//	delete this->m_state;
-//	this->m_state = new_state;
-//}
--- StarFish_Project/Player/PlayerBase/PlayerBase.h
+++ StarFish_Project/Player/PlayerBase/PlayerBase.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+<<<<<<< HEAD
 #include"../../Lib/D3D/D3D9.h"
 #include"../../Lib/Window/Window.h"
 #include"../../Lib/Texture/Texture.h"
@@ -22,35 +23,121 @@ public:
 	//-----------------------------------------------------
 
 	// 関数
+=======
+#include "../../Lib/D3D/D3D9.h"
+#include "../../Lib/Window/Window.h"
+#include "../../Lib/Texture/Texture.h"
+#include "../../Lib/Texture/TextureBoad2D.h"
+#include "../../Lib/Input/KeyBord.h"
+#include "../../GameObject/Object/Object.h"
+#include "../PlayerState/PlayerStateBase.h"
+
+// MEMO:自機1と2の操作が同一になっています（分割する必要あり）
+
+class PlayerBase : public Object {
+public:	
+>>>>>>> player(State)
 	// コンストラクタ
 	PlayerBase();
-
 	// 仮想デストラクタ
 	virtual ~PlayerBase() {}
 
+	// ゲームシーンで使用する関数 -------------------------
 	// 更新処理
 	// HACK：自機2も自機1の操作方法になっているので操作の分離が必要
+<<<<<<< HEAD
 	void Update()override;
 	
 	// 描画処理
 	// MEMO:自機2も自機1の画像を使用中、自機2の画像が完成次第変更する
 	void Draw()override;
+=======
+	void Update();
 
-	// ステート変更
-	void ChangeState(STATE* state);
+	// 描画処理
+	// MEMO:自機2も自機1の画像を使用中、自機2の画像が完成次第変更する
+	void Draw();
+	//-----------------------------------------------------
+>>>>>>> player(State)
 
-	D3DXVECTOR2 GetPos();
+	// 状態切り替え
+	void ChangeState(PlayerStateBase* state) {
+		m_state = state;
+		m_state->Init(this);
+	}
 
-	// 移動量ゲッター
-	D3DXVECTOR2 GetMovePos();
+	// 当たり判定で使用する関数	---------------------------
+	// プレイヤー座標ゲッター
+	D3DXVECTOR2 GetPos() {
+		return m_pos;
+	}
 
-	// 当たり判定用
-	// ポジションセッター
-	void SetPos(D3DXVECTOR2 pos);
+	// プレイヤー移動量ゲッター
+	D3DXVECTOR2 GetMovePos() {
+		return m_move;
+	}
 
-	// 移動量セッター
-	void SetMovePos(D3DXVECTOR2 move);
+	// プレイヤー座標セッター
+	void SetPos(D3DXVECTOR2 pos) {
+		m_pos = pos;
+	}
 
+	// プレイヤー移動量セッター
+	void SetMovePos(D3DXVECTOR2 move) {
+		m_move = move;
+	}
+	//-----------------------------------------------------
+
+
+	// 状態遷移（各State）で使用する関数 ------------------
+	// アニメーション番号ゲッター
+	int GetAnimationNumber() {
+		return m_animation_number;
+	}
+
+	// アニメーション番号0初期化
+	void ResetAnimationNumber() {
+		m_animation_number = 0;
+	}
+
+	// アニメーション番号インクリメント
+	void AddAnimationNumber() {
+		m_animation_number++;
+	}
+
+	// 状態画像セッター
+	void SetPlayerTexture(std::string new_player_texture) {
+		m_player_texture = new_player_texture;
+	}
+
+	// 状態遷移タイマーゲッター
+	int GetStateChangeTimer() {
+		return m_state_change_timer;
+	}
+
+	// 状態遷移タイマー0初期化
+	void ResetStateChangeTimer() {
+		m_state_change_timer = 0;
+	}
+
+	// 状態遷移タイマーインクリメント
+	void AddStateChangeTimer(){
+		++m_state_change_timer;
+	}
+
+
+	// 泳ぐ（ジャンプ）、傾いてる向きに移動
+	void SwimUp();
+
+	// 重力負荷
+	void AddGravity();
+
+	// X方向向き変更
+	void AngleAdjust(bool is_move_right);
+	//-----------------------------------------------------
+
+
+<<<<<<< HEAD
 	// アニメーション番号上書き（セッター）
 	void SetAnimationNumber(int new_animation_number);
 
@@ -59,24 +146,20 @@ public:
 		return PLAYER;
 	}
 	
+=======
+>>>>>>> player(State)
 protected:
 
 	//-----------------------------------------------------
 	// ゲーム内パラメータ用定数
-	// 重力負荷
-	const float GRAVITY = 1.f;
+	// 1フレームごとの画面下への移動量、重力負荷に使用
+	const float GRAVITY = 0.5f;
 
-	// 角度変換
+	// 向き変更時に1フレームごとに傾く角度
 	const float ANGLE_ADD = 0.5f;
 
-	// 泳ぎインターバル
-	const int SWIM_INTERVAL = 96;
-
-	// 向き変更時最大角度
+	// 向き変更時最大角度（ヒトデの頭の向きの左右の最大角度）
 	const float MAX_ANGLE = 45.f;
-
-	// 泳ぎアニメーション補助用
-	const int SWIM_ANIMATION_SUPPORT_NUMBER = 6;
 	//-----------------------------------------------------
 
 	//-----------------------------------------------------
@@ -93,46 +176,26 @@ protected:
 	// 分割画像Y枚数
 	const int TEXTURE_PARTITION_Y_NUMBER = 4;
 	//-----------------------------------------------------
-
-
-
-	// 関数
-	// 重力負荷
-	void AddGravity();
-
-	// X方向向き変更
-	void AngleAdjust(bool is_move_right);
 	
-
-	// 変数
-	// プレイヤー座標
-	D3DXVECTOR2 m_pos;
-
+	// 変数 ------------------------------------------------
 	// 画像格納用
-	// HACK:配列の方がいいかも
+	// HACK:自機が2種類分あるので工夫する必要がある
 	std::string m_player_texture;
 
 	// X、Y方向移動量
 	D3DXVECTOR2 m_move;
 
-	// 移動速度
-	float m_move_speed;
-
-	// 自機画像角度
+	// 自機画像角度（MAX_ANGLEから-MAX_ANGLE度まで）
 	float m_character_angle;
 
-	// 挙動のアニメーション番号管理用
+	// アニメーション番号（統合画像内のどの画像を描画するか）
 	int m_animation_number;
-
-	// 泳ぎクールタイム計測用
-	// HACK:Stateパターン内で管理する
-	int m_swim_interval_count;
+	//-----------------------------------------------------
 
 private:
-	// 関数
-	// 泳ぐ（ジャンプ）、傾いてる向きに移動
-	void SwimUp();
+	// 状態遷移用タイマー
+	int m_state_change_timer;
 
-	// ステートベースインスタンス
-	STATE m_state;
+	// ステート基底クラスのインスタンス
+	PlayerStateBase * m_state;
 };
