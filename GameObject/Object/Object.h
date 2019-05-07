56b4a3fcdc0a6c1f37e3f4d9bdd95f6f9b10b4a7
前川　洸太郎 <41092674+kou65@users.noreply.github.com>
2019-05-07 22:41:35 +0900

    オブジェクトを更新しました。

--- GameObject/Object/Object.h
+++ GameObject/Object/Object.h
@@ -9,40 +9,31 @@ class Object {
 public:
 
 	Object();
+	// 仮想デストラクタ
 	virtual ~Object() {};
-
-	/* 各アクセサ */
-
-	D3DXVECTOR2 GetPos() const{
-		return m_pos;
-	}
-	void SetPos(const D3DXVECTOR2&pos) {
-		m_pos = pos;
-	}
-
-	int GetId() const {
-		return m_id;
-	}
-	void SetId(int id) {
-		m_id = id;
-	}
-
-	bool GetIsActive()const {
-		return m_is_active;
-	}
-
 	// 仮想関数
 	virtual void Update() = 0;
 	virtual void Draw() = 0;
 
+	// 描画関数
+	void AnimationDraw(int max_animation, int anim_cange_time);
+
+	/* ゲッター */
+	D3DXVECTOR2 GetPos()const;
+	int GetId()const;
+	bool IsActive()const;
+
+	/* セッター */
+	void SetPos(const D3DXVECTOR2&pos);
+	void SetId(int id);
+
 protected:
 
-	// 位置
-	D3DXVECTOR2 m_pos;
-	// 生きているか
-	bool m_is_active;
+	D3DXVECTOR2 m_pos;     // 位置
+	bool m_is_active;      // Objectが活動しているかの状態変数
 	
 private:
-	// 生成id入れ
-	int m_id;
+	int m_id;              // 生成id
+	int m_animation_timer; // アニメーションの時間 
+	int m_animation_num;   // 現在のアニメーションの数
 };
