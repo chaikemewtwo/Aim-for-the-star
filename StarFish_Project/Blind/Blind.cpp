f386826e8dbed4bc7e15b1fb4c3115462040d1ee
福本萌々恵 <hakvt0.89io@gmail.com>
2019-05-11 00:40:53 +0900

    Blindクラスを追加(途中)

---
+++ StarFish_Project/Blind/Blind.cpp
@@ -0,0 +1,22 @@
+﻿#include"Blind.h"
+
+
+Blind::Blind(D3DXVECTOR2 pos, ObjectManager* obj_mng) {
+	m_pos = pos;
+	m_pobj_mng = obj_mng;
+	m_pobj_mng->Entry(this);
+}
+
+void Blind::Update() {
+	
+}
+
+void Blind::Draw() {
+	// ブラインドの小魚を描画(アニメーションあり)
+}
+
+void Blind::Delete() {
+	if (IsActive() == false) {
+		m_pobj_mng->Exit(this->GetId());
+	}
+}
---
+++ StarFish_Project/Blind/Blind.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include"../GameObject/Object/Object.h"
+#include"../GameObject/ObjectManager/ObjectManager.h"
+
+
+class Blind :public Object {
+public:
+	Blind(D3DXVECTOR2 pos, ObjectManager* obj_mng);
+	~Blind() {}
+	
+	void Update();
+	void Draw();
+	//void Create(D3DXVECTOR2 pos);
+	void Delete();
+
+private:
+	float m_speed;
+
+	ObjectManager* m_pobj_mng;
+};
