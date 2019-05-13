149b5b35cc164bfb0e24311da32f79fbd347eebf
福本萌々恵 <hakvt0.89io@gmail.com>
2019-05-13 20:03:57 +0900

    ブラインドの挙動を現在地から指定された目的地に行く挙動に変更（ラジアンを返す関数とUpdate内）、削除させる関数を修正（画面外の一定の距離で削除フラグをたたせる関数）

--- StarFish_Project/Blind/Blind.cpp
+++ StarFish_Project/Blind/Blind.cpp
@@ -1,86 +1,60 @@
 ﻿#include<cmath>
 #include"Blind.h"
 #include"../Lib/Texture/TextureBoad2D.h"
-#include"../Lib/Window/Window.h"
 
 
 Blind::Blind() {
 	m_speed = 5;
 	m_angle = 0;
-	
 	m_anim_change_time = 20;
 	m_max_animation = 2;
 	m_pos_direction = DIRECTION_NUM;
-	m_delete_timer = 60;
 }
 //―――――――――――――――――――――――
 
-void Blind::Create(D3DXVECTOR2 pos) {
+void Blind::Create(D3DXVECTOR2 pos, D3DXVECTOR2 goal) {
+	// 生成位置と目的地を設定
 	m_pos = pos;
-	//m_angle += GetRad();
+	m_goal_pos = goal;
+	// 移動方向に向かせたい《途中》
+	m_angle = (GetRad()*180)/PI;
+
 	// 現在の位置に当てはまるものを代入
 	if (m_pos.x < (WINDOW_W_F / 2) && m_pos.y < (WINDOW_H_F / 2)) {
 		m_pos_direction = TOP_LEFT;
-		m_angle = 190;
 	}
 	else if (m_pos.x > (WINDOW_W_F / 2) && m_pos.y < (WINDOW_H_F / 2)) {
 		m_pos_direction = TOP_RIGHT;
-		m_angle = -30;
 	}
 	else if (m_pos.x<(WINDOW_W_F / 2) && m_pos.y>(WINDOW_H_F / 2)) {
 		m_pos_direction = UNDER_LEFT;
-		m_angle = 160;
 	}
 	else if (m_pos.x > (WINDOW_W_F / 2) && m_pos.y > (WINDOW_H_F / 2)) {
 		m_pos_direction = UNDER_RIGHT;
-		m_angle = 30;
 	}
 }
 //―――――――――――――――――――――――
 
 void Blind::Update() {
-	m_move.x = 0;
-	m_move.y = 0;
-	OutScreen();
-	switch (m_pos_direction) {
-	case TOP_LEFT:
-		//m_move.x += sin(m_angle * PI / (float)360.f) * m_speed;
-		//m_move.y -= cos(m_angle * PI / (float)180.f) * m_speed;
-		m_pos.x += m_speed;
-		m_pos.y += m_speed / 2;
-		break;
-	case TOP_RIGHT:		
-		m_pos.x -= m_speed;
-		m_pos.y += m_speed / 2;
-		break;
-	case UNDER_LEFT:
-		m_pos.x += m_speed;
-		m_pos.y -= m_speed / 2;
-		break;
-	case UNDER_RIGHT:
-		m_pos.x -= m_speed;
-		m_pos.y -= m_speed / 2;
-		break;
-	}
-	//m_pos += m_move;
-}
+	DeleteJudg();
 
+	// 現在地と目的地のラジアンを求める
+	float rad = GetRad();
+	// 受け取ったラジアンで、目的地に向けて移動
+	m_pos.y += sinf(rad)*m_speed;
+	m_pos.x += cosf(rad)*m_speed;
+}
 //―――――――――――――――――――――――
 
-void Blind::OutScreen() {
-	// 画面外に出たら、削除までの時間をカウントダウン
-	if (m_pos.y > WINDOW_H_F || m_pos.x<0 || m_pos.x>WINDOW_W_F) {
-		if (m_delete_timer >= 0) {
-			m_delete_timer--;
-			// 時間が0になったら削除フラグをtrueに
-			if (m_delete_timer <= 0) {
-				m_is_active = false;
-			}
-		}
-	}
-	// 画面内に戻ったら時間を戻す
-	else if (m_pos.y < WINDOW_H_F || m_pos.x>0 || m_pos.x < WINDOW_W_F) {
-		m_delete_timer = 60;
+float Blind::GetRad() {
+	float num = atan2((m_goal_pos.y - m_pos.y), (m_goal_pos.x - m_pos.x));
+	return num;
+}
+//―――――――――――――――――――――――
+
+void Blind::DeleteJudg() {
+	if (m_pos.x<DELETE_WIDE_MIN || m_pos.x>DELETE_WIDE_MAX) {
+		m_is_active = false;
 	}
 }
 //―――――――――――――――――――――――
--- StarFish_Project/Blind/Blind.h
+++ StarFish_Project/Blind/Blind.h
@@ -2,10 +2,12 @@
 
 #include"../GameObject/Object/Object.h"
 #include"../GameObject/ObjectManager/ObjectManager.h"
+#include"../Lib/Window/Window.h"
 
 
 class Blind :public Object {
 public:
+	// 位置方向を指す定数　《変更予定》
 	enum Direction {
 		TOP_RIGHT,
 		TOP_LEFT,
@@ -17,22 +19,24 @@ public:
 	Blind();
 	~Blind() {}
 	
-	void Create(D3DXVECTOR2 pos);
+	void Create(D3DXVECTOR2 pos, D3DXVECTOR2 goal);
 	void Update();
 	void Draw();
-	// 仮の削除判定
-	void OutScreen();
+	void DeleteJudg();
+	float GetRad();
 
 private:
-	float m_speed;
-	D3DXVECTOR2 m_move;
-	float m_angle;
-	int m_anim_change_time;
-	int m_max_animation;
-	Direction m_pos_direction;
-	int m_delete_timer;
+	float m_speed;				// 移動速度
+	float m_angle;				// 角度
+	D3DXVECTOR2 m_goal_pos;		// 目的地の座標
+	Direction m_pos_direction;	// 現在の描画位置方向
+	int m_anim_change_time;		// 1コマのアニメーションの時間
+	int m_max_animation;		// 描画するアニメーションの数
 
-	ObjectManager* m_pobj_mng;
+	ObjectManager* m_pobj_mng;	
 
 	const int TEX_PARTITION_NUM = 2;
+	// 生存範囲のサイズ(仮の数値を代入)
+	const float DELETE_WIDE_MIN = -200;
+	const float DELETE_WIDE_MAX = WINDOW_W_F + 200;
 };
