def7bf382a1acabcb117b57592d0121c87d50247
福本萌々恵 <hakvt0.89io@gmail.com>
2019-05-14 12:03:02 +0900

    ブラインドクラスの不要部分を削除、その他細かい部分の修正

--- StarFish_Project/Blind/Blind.cpp
+++ StarFish_Project/Blind/Blind.cpp
@@ -8,7 +8,6 @@ Blind::Blind() {
 	m_angle = 0;
 	m_anim_change_time = 20;
 	m_max_animation = 2;
-	m_pos_direction = DIRECTION_NUM;
 }
 //―――――――――――――――――――――――
 
@@ -16,22 +15,9 @@ void Blind::Create(D3DXVECTOR2 pos, D3DXVECTOR2 goal) {
 	// 生成位置と目的地を設定
 	m_pos = pos;
 	m_goal_pos = goal;
-	// 移動方向に向かせたい《途中》
-	m_angle = (GetRad()*180)/PI;
 
-	// 現在の位置に当てはまるものを代入
-	if (m_pos.x < (WINDOW_W_F / 2) && m_pos.y < (WINDOW_H_F / 2)) {
-		m_pos_direction = TOP_LEFT;
-	}
-	else if (m_pos.x > (WINDOW_W_F / 2) && m_pos.y < (WINDOW_H_F / 2)) {
-		m_pos_direction = TOP_RIGHT;
-	}
-	else if (m_pos.x<(WINDOW_W_F / 2) && m_pos.y>(WINDOW_H_F / 2)) {
-		m_pos_direction = UNDER_LEFT;
-	}
-	else if (m_pos.x > (WINDOW_W_F / 2) && m_pos.y > (WINDOW_H_F / 2)) {
-		m_pos_direction = UNDER_RIGHT;
-	}
+	// 移動方向に画像が向いているように設定(Draw2D使用のために角度に直す)
+	m_angle = ((GetRadian(m_pos.x, m_pos.y, m_goal_pos.x, m_goal_pos.y) * 180) / PI) + 180;
 }
 //―――――――――――――――――――――――
 
@@ -39,27 +25,34 @@ void Blind::Update() {
 	DeleteJudg();
 
 	// 現在地と目的地のラジアンを求める
-	float rad = GetRad();
+	float radian = GetRadian(m_pos.x, m_pos.y, m_goal_pos.x, m_goal_pos.y);
+
 	// 受け取ったラジアンで、目的地に向けて移動
-	m_pos.y += sinf(rad)*m_speed;
-	m_pos.x += cosf(rad)*m_speed;
+	m_pos.y += sinf(radian)*m_speed;
+	m_pos.x += cosf(radian)*m_speed;
 }
 //―――――――――――――――――――――――
 
-float Blind::GetRad() {
-	float num = atan2((m_goal_pos.y - m_pos.y), (m_goal_pos.x - m_pos.x));
-	return num;
+// 引数に求めたい角度の2点を入れる　(第1引数＝現在地、第二引数＝目的地)
+float Blind::GetRadian(float pos_x, float pos_y, float x, float y) {
+	float rad = atan2((y - pos_y), (x - pos_x));
+	return rad;
 }
 //―――――――――――――――――――――――
 
+// 指定した左右位置(定数)を超えたら削除フラグをたたせる
 void Blind::DeleteJudg() {
 	if (m_pos.x<DELETE_WIDE_MIN || m_pos.x>DELETE_WIDE_MAX) {
 		m_is_active = false;
+	}
+	else if (m_pos.x > DELETE_WIDE_MIN || m_pos.x < DELETE_WIDE_MAX) {
+		m_is_active = true;
 	}
 }
 //―――――――――――――――――――――――
 
 void Blind::Draw() {
+
 	// ブラインドの小魚を描画(アニメーションあり)
 	Texture::Draw2D(
 		"Resource/hora_ready.png",
--- StarFish_Project/Blind/Blind.h
+++ StarFish_Project/Blind/Blind.h
@@ -7,36 +7,31 @@
 
 class Blind :public Object {
 public:
-	// 位置方向を指す定数　《変更予定》
-	enum Direction {
-		TOP_RIGHT,
-		TOP_LEFT,
-		UNDER_RIGHT,
-		UNDER_LEFT,
-		DIRECTION_NUM
-	};
-
 	Blind();
-	~Blind() {}
+	~Blind()override {}
 	
 	void Create(D3DXVECTOR2 pos, D3DXVECTOR2 goal);
-	void Update();
-	void Draw();
-	void DeleteJudg();
-	float GetRad();
+	void Update()override;
+	void Draw()override;
 
 private:
+	// 削除フラグ判定関数
+	void DeleteJudg();
+	// 2点の間の角度(ラジアン)を求める関数
+	float GetRadian(float pos_x, float pos_y, float x, float y);
+
+// 変数・定数 //
 	float m_speed;				// 移動速度
 	float m_angle;				// 角度
 	D3DXVECTOR2 m_goal_pos;		// 目的地の座標
-	Direction m_pos_direction;	// 現在の描画位置方向
 	int m_anim_change_time;		// 1コマのアニメーションの時間
 	int m_max_animation;		// 描画するアニメーションの数
 
 	ObjectManager* m_pobj_mng;	
 
 	const int TEX_PARTITION_NUM = 2;
-	// 生存範囲のサイズ(仮の数値を代入)
+
+	// 生存範囲のサイズ《現在は仮の数値を代入》
 	const float DELETE_WIDE_MIN = -200;
 	const float DELETE_WIDE_MAX = WINDOW_W_F + 200;
 };
