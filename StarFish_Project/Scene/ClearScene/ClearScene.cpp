b0912217bb136cc70a68939a2d34a20260f6db4b
福本萌々恵 <hakvt0.89io@gmail.com>
2019-05-27 13:49:20 +0900

    クリア画面の内部処理、プレイヤーの移動と背景のスクロールの実装（未リファクタ）

--- StarFish_Project/Scene/ClearScene/ClearScene.cpp
+++ StarFish_Project/Scene/ClearScene/ClearScene.cpp
@@ -1,17 +1,18 @@
 ﻿#include"ClearScene.h"
 #include"../../Lib/Texture/TextureBoad2D.h"
+#include"../../Lib/Window/Window.h"
 
 
 Clear::Clear() {
 	m_scene_step = INIT;
 
 	// プレイヤー1の画像登録
-	m_player1_texture_list[FLIGHT_TEXTURE] = "Resource/Texture/Player/de_crear_01.png";
-	m_player1_texture_list[CLEAR_POSE_TEXTURE] = "Resource/Texture/Player/de_crear_02.png";
+	m_player1_texture_list[FLIGHT_TEXTURE] = "Resource/Texture/Player/hi_clear_01.png";
+	m_player1_texture_list[CLEAR_POSE_TEXTURE] = "Resource/Texture/Player/hi_clear_02.png";
 
 	// プレイヤー2の画像登録
-	m_player2_texture_list[FLIGHT_TEXTURE] = "Resource/Texture/Player/hi_crear_01.png";
-	m_player2_texture_list[CLEAR_POSE_TEXTURE] = "Resource/Texture/Player/hi_crear_02.png";
+	m_player2_texture_list[FLIGHT_TEXTURE] = "Resource/Texture/Player/de_clear_01.png";
+	m_player2_texture_list[CLEAR_POSE_TEXTURE] = "Resource/Texture/Player/de_clear_02.png";
 
 	// 背景の画像登録
 	m_background_texture_list[SEA_TEXTURE] = "Resource/Texture/Map/bg_clear_01.png";
@@ -24,16 +25,60 @@ void Clear::Init() {
 	SceneBase::m_scene_id = CLEAR;
 	m_scene_step = UPDATE;
 
+	// 画像の初期化
 	m_player1_texture = m_player1_texture_list[FLIGHT_TEXTURE];
 	m_player2_texture = m_player2_texture_list[FLIGHT_TEXTURE];
-	m_background_texture = m_background_texture_list[SEA_TEXTURE];
+	m_background_texture1 = m_background_texture_list[SEA_TEXTURE];
+	m_background_texture2 = m_background_texture_list[SKY_TEXTURE];
 	
-	m_p1_pos = D3DXVECTOR2(760, 1000);
-	m_p2_pos = D3DXVECTOR2(1160, 1000);
+	// 座標の初期化
+	m_player1_pos = D3DXVECTOR2((WINDOW_W_F/2)-256, 1000);
+	m_player2_pos = D3DXVECTOR2((WINDOW_W_F/2)+256, 1000);
+	m_background1_pos = D3DXVECTOR2(0, (WINDOW_H_F-BACKGROUND_TEXTURE_SIZE_Y));
+	m_background2_pos = D3DXVECTOR2(0,m_background1_pos.y-BACKGROUND_TEXTURE_SIZE_Y);
+
+	// プレイヤーの変数初期化
+	m_player_animation_finish = false;
+	m_player_move_speed = 5;
+	m_player_animation_max = 16;
+	m_player_animation_num = 0;
+	m_player_animation_change_time = 5;
+	m_player_animation_timer = 0;
+
+	// 背景の変数初期化
+	m_is_background_move = false;
+	m_background_move_speed = 10;
 }
 //――――――――――――――――――――――――――――――――
 
 void Clear::Update() {
+	// プレイヤーの移動
+	if (m_player1_pos.y >= 600 && m_player2_pos.y >= 600) {
+		m_player1_pos.y -= m_player_move_speed;
+		m_player2_pos.y -= m_player_move_speed;
+
+		if (m_player1_pos.y <= 850 && m_player2_pos.y <= 850) {
+			m_is_background_move = true;
+			m_player_move_speed -= 0.01f;
+		}
+	}
+	
+	// 背景の移動
+	if (m_is_background_move) {
+		m_background1_pos.y += m_background_move_speed;
+		m_background2_pos.y += m_background_move_speed;
+
+		// 1枚目が描画し終わったら、3枚目の画像を入れる
+		if (m_background1_pos.y >= BACKGROUND_TEXTURE_SIZE_Y) {
+			m_background_texture1 = m_background_texture_list[MOON_TEXTURE];
+			m_background1_pos.y = m_background2_pos.y-BACKGROUND_TEXTURE_SIZE_Y;
+		}
+		// 2枚目を描画し終わったら移動を止める
+		else if (m_background2_pos.y >= BACKGROUND_TEXTURE_SIZE_Y) {
+			m_is_background_move = false;
+		}
+	}
+
 	if (m_pkey_bord.press(VK_SPACE)) {
 		m_scene_step = END;
 		m_scene_id = TITLE;
@@ -44,22 +89,62 @@ void Clear::Update() {
 void Clear::Draw() {
 	// 背景の描画
 	Texture::Draw2D(
-		m_background_texture.c_str(),
-		0, 0
+		m_background_texture1.c_str(),
+		m_background1_pos.x,m_background1_pos.y
+	);
+	Texture::Draw2D(
+		m_background_texture2.c_str(),
+		m_background2_pos.x, m_background2_pos.y
 	);
 
 	// プレイヤーの描画
 	Texture::Draw2D(
 		m_player1_texture.c_str(),
-		m_p1_pos.x, m_p1_pos.y,
-		0.5, 0.5, 0, 0.5, 0.5,
-		true, 4, 4, 0
+		m_player1_pos.x, m_player1_pos.y,
+		0.25, 0.25, 0, 0.5, 0.5,
+		true, PLAYER_TEXTURE_PARTITION_NUM, PLAYER_TEXTURE_PARTITION_NUM, 
+		m_player_animation_num
 	);
 	Texture::Draw2D(
 		m_player2_texture.c_str(),
-		m_p2_pos.x, m_p2_pos.y,
-		0.5, 0.5, 0, 0.5, 0.5,
-		true, 4, 4, 0
+		m_player2_pos.x, m_player2_pos.y,
+		0.25, 0.25, 0, 0.5, 0.5,
+		true, PLAYER_TEXTURE_PARTITION_NUM, PLAYER_TEXTURE_PARTITION_NUM, 
+		m_player_animation_num
 	);
+	PlayerAnimation();
+}
+//―――――――――――――――――――――――――――――――――
+
+void Clear::PlayerAnimation() {
+
+	// アニメーションが回り切っていたら
+	if (m_player_animation_finish == true && m_player_animation_num == 15) {
+		m_player_animation_num = 15;
+	}
+	// 回り切っていなければアニメーションする
+	else{
+		if (m_player_animation_timer >= m_player_animation_change_time) {
+
+			m_player_animation_timer = 0;
+			m_player_animation_num++;
+
+			// 背景が途中の場合、アニメーションをループさせる
+			if (m_background2_pos.y <= WINDOW_H_F && m_player_animation_num == 13) {
+				m_player_animation_num = 5;
+			}
+			// 背景スクロール後、アニメーションを次に遷移させる
+			else if (m_player_animation_num >= m_player_animation_max && m_player_animation_finish == false) {
+
+				m_player1_texture = m_player1_texture_list[CLEAR_POSE_TEXTURE];
+				m_player2_texture = m_player2_texture_list[CLEAR_POSE_TEXTURE];
+				m_player_animation_num = 0;
+				m_player_animation_finish = true;
+			}
+		}
+		else {
+			m_player_animation_timer++;
+		}
+	}
 }
 //―――――――――――――――――――――――――――――――――
--- StarFish_Project/Scene/ClearScene/ClearScene.h
+++ StarFish_Project/Scene/ClearScene/ClearScene.h
@@ -27,20 +27,37 @@ private:
 	void Init()override;
 	void Update()override;
 	void Draw()override;
+	//void AnimationDraw(std::string animation_textur, int max_animation, int change_time, int new_texture);
+	void PlayerAnimation();
 
 private:
-	D3DXVECTOR2 m_p1_pos;
-	D3DXVECTOR2 m_p2_pos;
+	int m_player_animation_timer;				// プレイヤーのカウント用
+	int m_player_animation_change_time;			// アニメーションの遷移時間
+	int m_player_animation_num;					// プレイヤーのアニメーション番号
+	int m_player_animation_max;					// プレイヤーのアニメーション数
+	float m_player_move_speed;					// プレイヤーの移動速度
+	float m_background_move_speed;				// 背景の移動速度
+	bool m_is_background_move;					// 背景を動かすフラグ
+	bool m_player_animation_finish;
+
+	D3DXVECTOR2 m_background1_pos;				// 背景の位置１
+	D3DXVECTOR2 m_background2_pos;				// 背景の位置２
+	D3DXVECTOR2 m_player1_pos;					// プレイヤーの位置１
+	D3DXVECTOR2 m_player2_pos;					// プレイヤーの位置2
 
 	// 各画像用の変数
 	std::string m_player1_texture;
 	std::string m_player2_texture;
-	std::string m_background_texture;
+	std::string m_background_texture1;
+	std::string m_background_texture2;
 
 	// 各画像のリスト
 	std::string m_player1_texture_list[CLEAR_TEXTURE_MAX];
 	std::string m_player2_texture_list[CLEAR_TEXTURE_MAX];
 	std::string m_background_texture_list[BG_TEXTURE_MAX];
+
+	const float BACKGROUND_TEXTURE_SIZE_Y = 1180;	// 背景画像のｙ軸のサイズ
+	const int PLAYER_TEXTURE_PARTITION_NUM = 4;		// プレイヤーの画像分割数
 
 	// エフェクトの登録
 	const std::string m_clear_effect = "Resource/Texture/Effect/crear_eff.png";
