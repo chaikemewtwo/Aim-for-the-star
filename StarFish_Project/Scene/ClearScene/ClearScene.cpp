3d4caaf9f482e940d3462cb78c916b6a4c9746fb
福本萌々恵 <hakvt0.89io@gmail.com>
2019-06-11 19:53:08 +0900

    マジックナンバーの定数化

--- StarFish_Project/Scene/ClearScene/ClearScene.cpp
+++ StarFish_Project/Scene/ClearScene/ClearScene.cpp
@@ -5,7 +5,7 @@
 
 Clear::Clear() {
 
-	m_scene_step = INIT;
+	m_scene_step = SceneStep::INIT;
 
 	// プレイヤー1の画像登録
 	m_player1_texture_list[FLY_TEXTURE] = "Resource/Texture/Player/hi_clear_01.png";
@@ -21,15 +21,15 @@ Clear::Clear() {
 	m_background_texture_list[TEXTURE_3] = "Resource/Texture/Map/bg_clear_03.png";
 
 	// サウンドの登録
-	m_fly_sound = m_paudio.getBuffer("Resource/Sound/Clear/player_fly.wav");
-	m_effect_sound = m_paudio.getBuffer("Resource/Sound/Clear/clear_effect.wav");
+	m_p_fly_sound = m_paudio.getBuffer("Resource/Sound/Clear/player_fly.wav");
+	m_p_effect_sound = m_paudio.getBuffer("Resource/Sound/Clear/clear_effect.wav");
 }
 //――――――――――――――――――――――――――――――――
 
 void Clear::Init() {
 
-	SceneBase::m_scene_id = CLEAR;
-	m_scene_step = UPDATE;
+	m_scene_id = SceneId::CLEAR;
+	m_scene_step = SceneStep::UPDATE;
 
 	// 画像の初期化
 	m_player1_texture = m_player1_texture_list[FLY_TEXTURE];
@@ -79,33 +79,33 @@ void Clear::Update() {
 
 	if (m_player1_pos.y >= PLAYER_FINISH_POS_Y && m_player2_pos.y >= PLAYER_FINISH_POS_Y) {
 		// プレイヤーの移動
-		PlayerMove();
+		MovePlayer();
 	}
 
 	if (m_is_background_move == true) {
 
 		// 移動時のサウンド再生
-		m_fly_sound->Play(0, 0, 0);
+		m_p_fly_sound->Play(0, 0, 0);
 
 		// 背景の移動
-		BackGroundMove();
+		ScrollBackGround();
 	}
 
 	// エフェクトが始まるタイミングでSE再生
 	if (m_player_animation_num == PLAYER_CLEARPOSE_START_NUM && m_player_animation_finish == true) {
-		m_effect_sound->Play(0, 0, 0);
+		m_p_effect_sound->Play(0, 0, 0);
 	}
 
 	// UIを描画サイズまで徐々に大きくする 
-	ClearUiSizeChange();
+	SizeChangeClearUi();
 
 	// 時間経過でタイトルに遷移
 	if (m_player_animation_num >= (m_player_animation_max - 1)) {
 
 		if (m_scene_change_time <= m_scene_change_count_timer) {
 
-			m_scene_step = END;
-			m_new_scene_id = TITLE;
+			m_scene_step = SceneStep::END;
+			m_new_scene_id = SceneId::TITLE;
 		}
 		else {
 			m_scene_change_count_timer++;
@@ -115,10 +115,10 @@ void Clear::Update() {
 	// デバック用
 	if (m_pkey_bord.press(VK_F1)) {
 
-		m_effect_sound->Stop();
-		m_fly_sound->Stop();
-		m_scene_step = END;
-		m_new_scene_id = TITLE;
+		m_p_effect_sound->Stop();
+		m_p_fly_sound->Stop();
+		m_scene_step = SceneStep::END;
+		m_new_scene_id = SceneId::TITLE;
 	}
 }
 //――――――――――――――――――――――――――――――――
@@ -135,7 +135,6 @@ void Clear::Draw() {
 		m_background2_pos.x, m_background2_pos.y
 	);
 
-	
 	if (m_player_animation_num >= PLAYER_CLEARPOSE_START_NUM && m_player_animation_finish == true) {
 
 		// エフェクトの描画
@@ -158,7 +157,7 @@ void Clear::Draw() {
 		// せいこうUIの描画
 		Texture::Draw2D(
 			m_clear_ui_texture.c_str(),
-			Window::WIDTH / 2, m_player1_pos.y - 500,
+			m_clear_ui_pos.x, m_clear_ui_pos.y,
 			m_clear_ui_size, m_clear_ui_size, 0, 0.5, 0.5
 		);
 	}
@@ -182,13 +181,13 @@ void Clear::Draw() {
 }
 //―――――――――――――――――――――――――――――――――
 
-void Clear::PlayerMove() {
+void Clear::MovePlayer() {
 
 		m_player1_pos.y -= m_player_move_speed;
 		m_player2_pos.y -= m_player_move_speed;
 
 		// 一定の位置で背景を動かす、プレイヤーの移動速度を減少
-		if (m_player1_pos.y <= 950 && m_player2_pos.y <= 950) {
+		if (m_player1_pos.y <= PLAYER_DECELERATION_POS_Y && m_player2_pos.y <= PLAYER_DECELERATION_POS_Y) {
 
 			m_is_background_move = true;
 			m_player_move_speed -= 0.01f;
@@ -196,7 +195,7 @@ void Clear::PlayerMove() {
 }
 //―――――――――――――――――――――――――――――――――
 
-void Clear::BackGroundMove() {
+void Clear::ScrollBackGround() {
 
 	m_background1_pos.y += m_background_move_speed;
 	m_background2_pos.y += m_background_move_speed;
@@ -214,13 +213,13 @@ void Clear::BackGroundMove() {
 }
 //―――――――――――――――――――――――――――――――――
 
-void Clear::ClearUiSizeChange() {
+void Clear::SizeChangeClearUi() {
 
 	if (m_player_animation_finish == true && m_player_animation_num >= PLAYER_CLEARPOSE_START_NUM && m_is_clear_ui_size_max == false && m_clear_ui_size <= 1) {
 
 		m_clear_ui_size += m_clear_ui_size_change_speed;
 		// 描画サイズまで達したら、サイズ変更の値を小さくする
-		if (m_clear_ui_size >= 1) {
+		if (m_clear_ui_size >= CLEAR_UI_SIZE_MAX) {
 
 			m_is_clear_ui_size_max = true;
 			m_clear_ui_size_change_speed = 0.001f;
@@ -230,7 +229,7 @@ void Clear::ClearUiSizeChange() {
 
 		m_clear_ui_size -= m_clear_ui_size_change_speed;
 
-		if (m_clear_ui_size <= CLEAR_UI_MIN_SIZE) {
+		if (m_clear_ui_size <= CLEAR_UI_SIZE_MIN) {
 			m_is_clear_ui_size_max = false;
 		}
 	}
--- StarFish_Project/Scene/ClearScene/ClearScene.h
+++ StarFish_Project/Scene/ClearScene/ClearScene.h
@@ -33,9 +33,12 @@ private:
 	void ChangePlayerAnimation();
 	// エフェクトのアニメーション処理
 	void ChangeEffectAnimation();
-	void PlayerMove();
-	void BackGroundMove();
-	void ClearUiSizeChange();
+	// クリアシーン内のプレイヤー移動
+	void MovePlayer();
+	// クリアシーン内の背景スクロール
+	void ScrollBackGround();
+	// クリアUIのサイズ変更
+	void SizeChangeClearUi();
 	
 private:
 	int m_scene_change_count_timer;		// 遷移までの時間カウント用
@@ -58,13 +61,18 @@ private:
 	std::string m_player1_texture_list[CLEAR_TEXTURE_MAX];
 	std::string m_player2_texture_list[CLEAR_TEXTURE_MAX];
 
-	const float PLAYER_FINISH_POS_Y = 750;			// プレイヤーの最終位置
-	const float PLAYER_DECELERATION_POS_Y = 950;	// プレイヤーの減速位置
-
-	const int PLAYER_CLEARPOSE_START_NUM = 6;		// ポーズの開始番号
-	const int PLAYER_REPEAT_ANIMATION_LAST_NUM = 13;// アニメーションの繰り返し位置の番号（終わり）
-	const int PLAYER_REPEAT_ANIMATION_START_NUM = 5;// アニメーションの繰り返し位置の番号（始まり） 
-	const int PLAYER_TEXTURE_PARTITION_NUM = 4;		// プレイヤーの画像分割数
+	// プレイヤーの最終位置
+	const float PLAYER_FINISH_POS_Y = 750;			
+	// プレイヤーの減速位置
+	const float PLAYER_DECELERATION_POS_Y = 950;	
+	// ポーズの開始番号
+	const int PLAYER_CLEARPOSE_START_NUM = 6;		
+	// アニメーションの繰り返し位置の番号（終わり）
+	const int PLAYER_REPEAT_ANIMATION_LAST_NUM = 13;
+	// アニメーションの繰り返し位置の番号（始まり） 
+	const int PLAYER_REPEAT_ANIMATION_START_NUM = 5;
+	// プレイヤーの画像分割数
+	const int PLAYER_TEXTURE_PARTITION_NUM = 4;		
 
 	//　背景　//
 	D3DXVECTOR2 m_background1_pos;		// 位置１
@@ -77,7 +85,8 @@ private:
 	std::string m_background_texture2;
 	std::string m_background_texture_list[BACKGROUND_TEXTURE_MAX];
 
-	const float BACKGROUND_TEXTURE_SIZE_Y = 1180;	// 背景画像のｙ軸のサイズ
+	// 背景画像のｙ軸のサイズ
+	const float BACKGROUND_TEXTURE_SIZE_Y = 1180;	
 
 	//　エフェクト　//
 	int m_effect_animation_num;			// アニメーション番号
@@ -85,8 +94,8 @@ private:
 	int m_effect_animation_timer;		// アニメーションの時間カウント用
 	int m_effect_animation_change_time;// アニメーションの遷移時間
 
-	const int EFFECT_TEXTURE_PARTITION_NUM = 4;		// エフェクトの画像分割数
-
+	// エフェクトの画像分割数
+	const int EFFECT_TEXTURE_PARTITION_NUM = 4;		
 	// エフェクトの登録
 	const std::string m_clear_effect = "Resource/Texture/Effect/crear_eff.png";
 
@@ -96,11 +105,13 @@ private:
 	float m_clear_ui_size_change_speed;	// サイズ変更の速度
 	bool m_is_clear_ui_size_max;		// 描画サイズが本来のサイズかどうか
 
-	const float CLEAR_UI_MIN_SIZE = 0.9f;			// UIの最小サイズ
+	// UIの最小サイズ
+	const float CLEAR_UI_SIZE_MIN = 0.9f;	
+	const float CLEAR_UI_SIZE_MAX = 1.f;
 	// UIの登録
 	const std::string m_clear_ui_texture = "Resource/Texture/UI/clear_logo.png";
 
 	// クリア時のサウンド
-	IDirectSoundBuffer8* m_fly_sound;
-	IDirectSoundBuffer8* m_effect_sound;
+	IDirectSoundBuffer8* m_p_fly_sound;
+	IDirectSoundBuffer8* m_p_effect_sound;
 };
