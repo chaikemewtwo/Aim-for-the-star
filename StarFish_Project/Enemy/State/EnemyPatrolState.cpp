b9f40a71037e97f2aa0147051d62172a63453cd4
福本萌々恵 <hakvt0.89io@gmail.com>
2019-06-16 14:29:25 +0900

    巡回用のStateを作成

---
+++ StarFish_Project/Enemy/State/EnemyPatrolState.cpp
@@ -0,0 +1,30 @@
+﻿#include<math.h>
+#include"EnemyPatrolState.h"
+#include"../Enemy/EnemyBase.h"
+
+
+Patrol* Patrol::GetInstance() {
+	static Patrol s_patrol;
+	return &s_patrol;
+}
+
+void Patrol::Action(EnemyBase* e) {
+
+	D3DXVECTOR2 pos = e->GetPos();
+
+	if (m_max_posx_count > m_posx_count && e->IsLeft() == false) {
+		pos.x -= sin(PI * 2 / 120 * m_posx_count)*e->GetSpeed();
+	}
+	else if (m_max_posx_count > m_posx_count && e->IsLeft() == true) {
+		pos.x += sin(PI * 2 / 120 * m_posx_count)*e->GetSpeed();
+
+	}
+	
+	m_posx_count++;
+
+	if (m_max_posx_count < m_posx_count) {
+		m_posx_count = 0;
+	}
+
+	e->SetPos(pos);
+}
---
+++ StarFish_Project/Enemy/State/EnemyPatrolState.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include"EnemyStateBase.h"
+
+
+class Patrol :public StateBase {
+public:
+	static Patrol* GetInstance();
+	void Action(EnemyBase* e)override;
+private:
+	Patrol() {}
+	~Patrol() {}
+
+private:
+	float m_posx_count;
+	const float m_max_posx_count = 50;
+};
